5bab24a1f7886927da7a2e89f37daa2227e72fde
Phanx-28751 <Phanx-28751@c6a9b991-949f-4857-8f54-20636e960553>
2010-10-23 08:24:58 +0000

    - Make it work - Add bindings.xml - Add externals

--- trunk/CancelMyBuffs.lua
+++ trunk/CancelMyBuffs.lua
@@ -5,7 +5,7 @@
 	http://www.wowinterface.com/addons/
 ----------------------------------------------------------------------]]
 
-local _, ns = ...
+local ADDON_NAME, ns = ...
 
 local L = setmetatable(ns.L or { }, { __index = function(t, k)
 	if k == nil then return "" end
@@ -14,53 +14,29 @@ local L = setmetatable(ns.L or { }, { __index = function(t, k)
 	return k
 end })
 
-L["SLOWFALL"] = GetSpellInfo(130)
-L["SHADOWMOURNE"] = GetItemInfo(49623) or L["SHADOWMOURNE"]
+L["Shadowmourne"] = GetItemInfo(49623) or L["Shadowmourne"]
+L["Slow Fall"] = GetSpellInfo(130)
 
 ------------------------------------------------------------------------
 
 local defaults = {
 	profile = {
-		{
-			name = L["Default"],
-			enable = true,
-			color = { 1, 1, 0 },
-			flash = false,
-			sound = false,
-			include = {
-				["INVULNERABLE"] = true,
-				["SHAPESHIFTED"] = true,
-				["SLOWFALL"] = true,
-			},
-		},
-		{
-			name = L["Cosmetic Effects"],
-			enable = true,
-			color = { 1, 1, 0 },
-			flash = false,
-			sound = false,
-			include = {
-				["COSMETIC"] = true,
-			},
-		},
-		{
-			name = L["Weapon Buffs"],
-			enable = true,
-			color = { 118 / 255, 47 / 255, 170 / 255 },
-			flash = false,
-			sound = false,
-			include = {
-				["WEAPONBUFFS"] = true,
-			},
+		forms = true,
+		mounts = true,
+		vehicles = true,
+		buffGroups = {
+			["Invulnerability Effects"] = true,
+			["Shapeshift Effects"] = true,
+			["Slow Fall"] = true,
 		},
 	},
 	global = {
-		["INVULNERABLE"] = {
+		["Invulnerability Effects"] = {
 			[19263] = true, -- Deterrence
 			[1022]  = true, -- Hand of Protection
 			[45438] = true, -- Ice Block
 		},
-		["SHAPESHIFTED"] = {
+		["Shapeshift Effects"] = {
 			[71485] = true, -- Agility of the Vrykul
 			[71556] = true, -- Agility of the Vrykul (heroic)
 			[71491] = true, -- Aim of the Iron Dwarves
@@ -75,9 +51,10 @@ local defaults = {
 			[22736] = true, -- Gordok Ogre Suit
 			[58501] = true, -- Iron Boot Flask
 			[16591] = true, -- Noggenfogger Elixir (skeleton)
+			[30167] = true, -- Red Ogre Costume (from Carved Ogre Idol)
 			[24740] = true, -- Wisp Costume
 		},
-		["COSMETIC"] = {
+		["Cosmetic Effects"] = {
 			[60122] = true, -- Baby Spice
 			[27571] = true, -- Cascade of Roses (from Handful of Roses)
 			[51010] = true, -- Dire Brew
@@ -92,17 +69,16 @@ local defaults = {
 			[24710] = true, -- Ninja Costume
 			[60106] = true, -- Old Spices
 			[24709] = true, -- Pirate Costume
-			[30167] = true, -- Red Ogre Costume (from Carved Ogre Idol) -- check prevents mounting?
 			[24723] = true, -- Skeleton Costume -- check prevents mounting?
 			[44755] = true, -- Snowflakes (from Handful of Snowflakes)
 			[61815] = true, -- Sprung! (from Spring Flowers)
 		},
-		["SLOWFALL"] = {
+		["Slow Fall"] = {
 			[1706]  = true, -- Levitate
 			[16593] = true, -- Noggenfogger Elixir (slow fall)
 			[130]   = true, -- Slow Fall
 		},
-		["SHADOWMOURNE"] = {
+		["Shadowmourne"] = {
 			[73422] = true, -- Chaos Bane (Shadowmourne proc) (ret paladins want to cancel it)
 		},
 	}
@@ -110,226 +86,73 @@ local defaults = {
 
 ------------------------------------------------------------------------
 
-local CancelMyBuffs = LibStub("AceAddon-3.0"):NewAddon("CancelMyBuffs", "AceEvent-3.0")
+local buffList = { }
+
+local CancelMyBuffs = LibStub("AceAddon-3.0"):NewAddon("CancelMyBuffs")
 
 function CancelMyBuffs:OnInitialize()
-	self.db = LibStub("AceDB-3.0"):New("CancelMyBuffsDB", { profile = defaults }, true)
+	self.db = LibStub("AceDB-3.0"):New("CancelMyBuffsDB", defaults, true)
 
-	self.buttons = { }
-	self.buttonPool = { }
+	self.button = CreateFrame("Button", "CancelMyBuffsButton", nil, "SecureActionButtonTemplate")
+	self.button:RegisterForClicks("AnyUp")
 end
 
 function CancelMyBuffs:OnEnable()
-	for _, bindGroup in ipairs(self.db.profile) do
-		if bindGroup.enable then
-			local macrotext = ""
-			for groupName in pairs(bindGroup.include) do
-				local buffGroup = self.db.buffGroups[groupName]
-				if buffGroup then
-					for buff in pairs(buffGroup) do
-						macrotext = macrotext .. "\n/cancelaura " .. buff
+	local _, class = UnitClass("player")
+
+	local macrotext = ""
+	if self.db.profile.forms and (class == "DRUID" or class == "PRIEST" or class == "SHAMAN") then
+		macrotext = macrotext .. "\n/cancelform"
+	end
+	if self.db.profile.mounts then
+		macrotext = macrotext .. "\n/dismount"
+	end
+	if self.db.profile.vehicles then
+		macrotext = macrotext .. [[\n/run if UnitHasVehicleUI("player")then VehicleExit()end]]
+	end
+	for group in pairs(self.db.profile.buffGroups) do
+		local buffs = self.db.global[group]
+		if group then
+			for id, enabled in pairs(buffs) do
+				if enabled then
+					local name, _, icon = GetSpellInfo(id)
+					if name then
+						buffList[name] = icon
 					end
-				elseif groupName == "MOUNTED" then
-					macrotext = macrotext .. "\n/dismount\n/run ExitVehicle()"
-				elseif groupName == "WEAPONBUFFS" then
-					macrotext = macrotext .. "\n/run CancelItemTempEnchantment(1) CancelItemTempEnchantment(2)"
 				end
-			end
-
-			macrotext = macrotext:sub(3)
-
-			if macrotext:len() < 1024 then
-				local button = CreateFrame("Button", "CancelMyBuffsButton" .. (#self.buttons + 1), UIParent, "SecureActionButtonTemplate, ActionButtonTemplate")
-				button:SetAttribute("type1", "macro")
-				button:SetAttribute("macrotext1", macrotext)
-				button:SetOverrideBinding()
-				self.buttons[#self.buttons + 1] = button
-				print("Successfully created binding for", name, "group.")
-			else
-				print("Could not create binding for", name, "group. Too many buffs selected.")
 			end
 		end
 	end
-
-	if #self.buttons > 0 then
-		self:RegisterEvent("UNIT_AURA")
+	for buff in pairs(buffList) do
+		macrotext = macrotext .. "\n/cancelaura " .. buff
 	end
+	macrotext = macrotext:sub(2)
+
+	if macrotext:len() > 1024 then
+		print("Too many buffs selected!")
+		macrotext = macrotext:sub(1, 1024):match("(.+)\n/[^\/]+$")
+	end
+
+	self.button:SetAttribute("type1", "macro")
+	self.button:SetAttribute("macrotext1", macrotext)
+
+	self:SetupOptions()
+
+	-- self:RegisterEvent("UNIT_AURA")
 end
 
 function CancelMyBuffs:UNIT_AURA(event, unit)
 	if unit ~= "player" then return end
 
-	for _, bindGroup in ipairs(self.db.profile) do
-		if bindGroup.enable then
-			local active
-			for groupName in pairs(bindGroup.include) do
-				local buffGroup = self.db.buffGroups[groupName]
-				if buffGroup then
-					for buff in pairs(buffGroup) do
-						if UnitBuff(unit, buff) then
-							active = true
-							break
-						end
-					end
-				end
-				if active then
-					break
-				end
-			end
-			if active then
-				self:Alert(bindGroup)
-			end
+	for buff in pairs(buffList) do
+		if UnitBuff(unit, buff) then
+			return self:Alert()
 		end
 	end
 end
 
 function CancelMyBuffs:Alert(bindGroup)
-	if not bindGroup.enable then return end
-
 	UIErrorsFrame:AddMessage(bindGroup.name .. " buff found! Press " .. bindGroup.bind .. " to cancel!", bindGroup.color[1] or 1, bindGroup.color[2] or 1, bindGroup.color[3] or 1)
-end
-
-------------------------------------------------------------------------
-
-function CancelMyBuffs:NewBindGroup(name)
-	if not name then return end
-	local groupName = name:gsub("%a", string.upper, 1)
-
-	local bindGroup = {
-		name = groupName,
-		type = "group", args = {
-			enable = true,
-			color = { 1, 1, 0 },
-			flash = true,
-			sound = "NONE",
-			include = { },
-		}
-	}
-
-	table.insert(self.db.profile, bindGroup)
-	self.options.args[groupName] = self:GetOptionsForBindGroup(bindGroup)
-end
-
-function CancelMyBuffs:GetOptionsForBindGroup(bindGroup)
-	local groupName = bindGroup.name
-	local option = {
-		name = groupName,
-		type = "group", guiInline = true, args = {
-			enable = {
-				order = 100,
-				name = L["Enable"],
-				desc = L["Enable this binding group."],
-				type = "toggle",
-				arg = groupName,
-				get = function(t)
-					for _, group in ipairs(self.db.profile) do
-						if group.name == t.arg then
-							return group.enable
-						end
-					end
-				end,
-				set = function(t, v)
-					for _, group in ipairs(self.db.profile) do
-						if group.name == t.arg then
-							group.enable = v
-						end
-					end
-				end,
-			},
-			color = {
-				order = 200,
-				name = L["Color"],
-				desc = L["Set the color for this binding group."],
-				type = "color",
-				arg = groupName,
-				get = function(t)
-					for _, group in ipairs(self.db.profile) do
-						if group.name == t.arg then
-							return group.color
-						end
-					end
-				end,
-				set = function(t, r, g, b)
-					for _, group in ipairs(self.db.profile) do
-						if group.name == t.arg then
-							group.color[1] = r
-							group.color[1] = g
-							group.color[1] = b
-						end
-					end
-				end,
-			},
-			flash = {
-				order = 300,
-				name = L["Flash"],
-				desc = L["Enable screen flashing for this binding group."],
-				type = "toggle",
-				arg = groupName,
-				get = function(t)
-					for _, group in ipairs(self.db.profile) do
-						if group.name == t.arg then
-							return group.flash
-						end
-					end
-				end,
-				set = function(t, v)
-					for _, group in ipairs(self.db.profile) do
-						if group.name == t.arg then
-							group.flash = v
-						end
-					end
-				end,
-			},
-			sound = {
-				order = 400,
-				type = "toggle",
-				arg = groupName,
-				get = function(t)
-					for _, group in ipairs(self.db.profile) do
-						if group.name == t.arg then
-							return group.sound
-						end
-					end
-				end,
-				set = function(t, v)
-					for _, group in ipairs(self.db.profile) do
-						if group.name == t.arg then
-							group.sound = v
-						end
-					end
-				end,
-			},
-			include = {
-				order = 500,
-				name = L["Include buff groups"],
-				desc = L["Select which buff groups to include in this binding group."],
-				type = "group", guiInline = true, args = {
-				},
-			},
-		},
-	}
-	for gname in pairs(self.db.global) do
-		options.args.include.args[gname] = {
-			name = gname,
-			desc = L["Include this buff group in this binding group."],
-			type = "toggle",
-			arg = groupName,
-			get = function(t)
-				for _, group in ipairs(self.db.profile) do
-					if group.name == t.arg then
-						return group.include[gname]
-					end
-				end
-			end,
-			set = function(t, v)
-				for _, group in ipairs(self.db.profile) do
-					if group.name == t.arg then
-						group.include[gname] = v
-					end
-				end
-			end,
-		}
-	end
 end
 
 ------------------------------------------------------------------------
@@ -337,39 +160,94 @@ end
 function CancelMyBuffs:SetupOptions()
 	if self.options then return end
 
+	local CANCELMYBUFFS_BINDING = "CLICK CancelMyBuffsButton:LeftButton"
+
 	self.options = {
 		name = "CancelMyBuffs",
 		desc = L["CancelMyBuffs lets you quickly cancel unwanted buffs."],
-		type = "group", args = {
-			new = {
-				order = 1,
-				name = L["Add new binding group"],
-				type = "input", usage = "Type a name for the new binding group",
-				set = function(_, text)
-					if not text or text:len() < 1 then
-						print("Invalid binding group name.")
-						return
-					end
-					for _, bindGroup in ipairs(self.db.profile) do
-						if bindGroup.name:lower() == text:lower() then
-							print("Binding group name already in use.")
-							return
-						end
-					end
-					self:NewBindGroup(name)
-				end
+		type = "group",
+		get = function(t) return self.db.profile[t[#t]] end,
+		set = function(t, v) self.db.profile[t[#t]] = v end,
+		args = {
+			keybinding = {
+				name = L["Key Binding"],
+				order = 100, width = "full",
+				type = "keybinding",
+				get = function()
+					return GetBindingKey(CANCELMYBUFFS_BINDING)
+				end,
+				set = function(_, v)
+					SetBinding(value, CANCELMYBUFFS_BINDING)
+					SaveBindings(GetCurrentBindingSet())
+				end,
+			},
+			mounts = {
+				name = L["Dismount"],
+				desc = L["Also dismount when cancelling buffs."],
+				order = 200, width = "double",
+				type = "toggle",
+			},
+			vehicles = {
+				name = L["Leave Vehicle"],
+				desc = L["Also leave vehicles when cancelling buffs."],
+				order = 300, width = "double",
+				type = "toggle",
+			},
+			buffGroups = {
+				name = L["Remove Buff Groups"],
+				desc = L["Select which buffs to remove."],
+				order = 500,
+				type = "group", inline = true,
+				get = function(t) return self.db.profile.buffGroups[t[#t]] end,
+				set = function(t, v) self.db.profile.buffGroups[t[#t]] = v end,
+				args = {
+				},
 			},
 		},
 	}
 
-	for _, bindGroup in ipairs(self.db.profile) do
-		self.options.args[bindGroup.name] = self:GetOptionsForBindGroup(bindGroup)
+	for groupName in pairs(self.db.global) do
+		self.options.args.buffGroups.args[groupName] = {
+			name = L[groupName],
+			width = "double",
+			type = "toggle",
+		}
 	end
 
-	local options2 = {
+	self.groupOptions = {
 		name = L["Buff Groups"],
-		desc = L["Use this panel to add new buffs to cancel."],
-		type = "group", args = {
+		type = "group",
+		args = {
 		},
 	}
+
+	for groupName, groupBuffs in pairs(self.db.global) do
+		local t = {
+			name = L[groupName],
+			type = "group",
+			args = {
+			}
+		}
+		for id in pairs(groupBuffs) do
+			local name, _, icon = GetSpellInfo(id)
+			if name then
+				t.args[name] = {
+					name = "|T" .. icon .. ":0|t " .. name,
+					type = "toggle",
+				}
+			end
+		end
+		self.groupOptions.args[groupName] = t
+	end
+
+	LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("CancelMyBuffs", self.options)
+
+	self.optionsPanel = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("CancelMyBuffs")
+
+	self.aboutPanel = LibStub("LibAboutPanel").new("CancelMyBuffs", "CancelMyBuffs")
+
+	SLASH_CANCELMYBUFFS1 = "/cmb"
+	SlashCmdList.CANCELMYBUFFS = function()
+		InterfaceOptionsFrame_OpenToCategory(self.optionsPanel)
+	end
 end
