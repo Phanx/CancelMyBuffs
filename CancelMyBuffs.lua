c5a7cfae40944044334024738d168108552ca9da
Phanx <addons@phanx.net>
2011-12-19 07:57:40 +0000

    - Updated Curse download URL

--- CancelMyBuffs.lua
+++ CancelMyBuffs.lua
@@ -3,9 +3,9 @@
 	Cancel unwanted buffs quickly and easily.
 	Written by Phanx <addons@phanx.net>
 	Maintained by Akkorian <akkorian@hotmail.com>
-	Copyright © 2010–2011 Phanx. Some rights reserved. See LICENSE.txt for details.
+	Copyright ï¿½ 2010ï¿½2011 Phanx. Some rights reserved. See LICENSE.txt for details.
 	http://www.wowinterface.com/downloads/info18447-CancelMyBuffs.html
-	http://wow.curse.com/downloads/wow-addons/details/cancelmybuffs.aspx
+	http://www.curse.com/addons/wow/cancelmybuffs
 ----------------------------------------------------------------------]]
 
 local ADDON_NAME, ns = ...
