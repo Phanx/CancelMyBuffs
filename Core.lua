2c38fc18aa9d4f167bdeb719b3b5d207af0a6c71
Phanx <addons@phanx.net>
2014-10-18 22:05:42 +0000

    - Add Geist Costume, Ghoul Costume, other Hallow's End costumes

--- Core.lua
+++ Core.lua
@@ -51,85 +51,91 @@ function addon:OnLoad()
 	if not next(self.db.global.buffGroups) then
 		local t = self.db.global.buffGroups
 		t["Invulnerability"] = {
-			[46924] = 46924, -- Blade Storm [WARRIOR]
-			[19263] = 19263, -- Deterrence [HUNTER]
-			[642]   = 642,   -- Divine Shield [PALADIN]
-			[1022]  = true,  -- Hand of Protection
-			[45438] = 45438, -- Ice Block [MAGE]
+			[46924]  = 46924, -- Blade Storm [WARRIOR]
+			[19263]  = 19263, -- Deterrence [HUNTER]
+			[642]    = 642,   -- Divine Shield [PALADIN]
+			[1022]   = true,  -- Hand of Protection
+			[45438]  = 45438, -- Ice Block [MAGE]
 		}
-		t["Stealth"] = {
-			[66]    = 66,    -- Invisibility [MAGE]
-			[5215]  = 5215,  -- Prowl [DRUID]
-			[58984] = 58984, -- Shadowmeld [NightElf]
-			[1784]  = 1784,  -- Stealth [ROGUE]
-			[1856]  = 1856,  -- Vanish [ROGUE] -- check 11327
+		t["Stealth"]  = {
+			[66]     = 66,    -- Invisibility [MAGE]
+			[5215]   = 5215,  -- Prowl [DRUID]
+			[58984]  = 58984, -- Shadowmeld [NightElf]
+			[1784]   = 1784,  -- Stealth [ROGUE]
+			[1856]   = 1856,  -- Vanish [ROGUE] -- check 11327
 		}
-		t["Controllers"] = {
-			[75111] = true, -- Blue Crashin' Thrashin' Racer Controller
-			[30019] = true, -- Control Piece (Karazhan)
-			[49352] = true, -- Crashin' Thrashin' Racer Controller
-			[45440] = true, -- Steam Tonk Controller
-			[65451] = true, -- Using MiniZep Controller
+		t["Controllers"]  = {
+			[75111]  = true, -- Blue Crashin' Thrashin' Racer Controller
+			[30019]  = true, -- Control Piece (Karazhan)
+			[49352]  = true, -- Crashin' Thrashin' Racer Controller
+			[45440]  = true, -- Steam Tonk Controller
+			[65451]  = true, -- Using MiniZep Controller
 		}
 		t["Shapeshift Effects"] = {
-			[24732] = true, -- Bat Costume
-			[22736] = true, -- Gordok Ogre Suit
-			[58501] = true, -- Iron Boot Flask
-			[16591] = true, -- Noggenfogger Elixir [skeleton]
-			[30167] = true, -- Red Ogre Costume [Carved Ogre Idol]
-			[61716] = true, -- Rabbit Costume [Blossoming Branch]
-			[24723] = true, -- Skeleton Costume
-			[21848] = true, -- Snowman
-			[24740] = true, -- Wisp Costume
+			[172010] = true, -- Abomination Costume
+			[24732]  = true, -- Bat Costume
+			[172015] = true, -- Geist Costume
+			[172008] = true, -- Ghoul Costume
+			[22736]  = true, -- Gordok Ogre Suit
+			[24927]  = true, -- Hallow's End Candy [floating spirit]
+			[58501]  = true, -- Iron Boot Flask
+			[16591]  = true, -- Noggenfogger Elixir [skeleton]
+			[30167]  = true, -- Red Ogre Costume [Carved Ogre Idol]
+			[61716]  = true, -- Rabbit Costume [Blossoming Branch]
+			[24723]  = true, -- Skeleton Costume
+			[172003] = true, -- Slime Costume
+			[21848]  = true, -- Snowman
+			[172020] = true, -- Skeleton Costume
+			[24740]  = true, -- Wisp Costume
 		}
-		t["Quest Effects"] = {
-			[37868] = true, -- Arcano-Scorp Control [Shadowmoon Valley]
-			[77647] = true, -- Fel-Infused [killing mobs in Dustwind Cave]
-			[88715] = true, -- Herbouflage [Lifegiving Seed]
-			[75724] = true, -- Seeds of Discord [Ogre Disguise quest item]
-			[52191] = true, -- Scourge Disguize [Zul'Drak]
-			[43369] = true, -- Worg Disguise [Howling Fjord]
+		t["Quest Effects"]  = {
+			[37868]  = true, -- Arcano-Scorp Control [Shadowmoon Valley]
+			[77647]  = true, -- Fel-Infused [killing mobs in Dustwind Cave]
+			[88715]  = true, -- Herbouflage [Lifegiving Seed]
+			[75724]  = true, -- Seeds of Discord [Ogre Disguise quest item]
+			[52191]  = true, -- Scourge Disguize [Zul'Drak]
+			[43369]  = true, -- Worg Disguise [Howling Fjord]
 		}
-		t["Cosmetic Effects"] = {
-			[60122] = true, -- Baby Spice
-			[27571] = true, -- Cascade of Roses [Handful of Roses]
-			[51010] = true, -- Dire Brew
-			[24735] = true, -- Ghost Costume
-			[44185] = true, -- Jack-o'-Lanterned! [cast by Headless Horseman]
-			[44212] = true, -- Jack-o'-Lanterned! [Weighted Jack-o'-Lantern]
-			[24712] = true, -- Leper Gnome Costume
-			[61819] = true, -- Manabonked! [The Mischief Maker]
-			[61834] = true, -- Manabonked! [cast by Minigob Manabonk]
-			[69285] = true, -- Mohawked! -- check 58493
-			[42365] = true, -- Murloc Costume
-			[24710] = true, -- Ninja Costume
-			[60106] = true, -- Old Spices
-			[24709] = true, -- Pirate Costume
-			[26157] = true, -- PX-238 Winter Wondervolt
-			[44755] = true, -- Snowflakes [Handful of Snowflakes]
-			[61815] = true, -- Sprung! [Spring Flowers]
-			[61781] = true, -- Turkey Feathers
+		t["Cosmetic Effects"]  = {
+			[60122]  = true, -- Baby Spice
+			[27571]  = true, -- Cascade of Roses [Handful of Roses]
+			[51010]  = true, -- Dire Brew
+			[24735]  = true, -- Ghost Costume
+			[44185]  = true, -- Jack-o'-Lanterned! [cast by Headless Horseman]
+			[44212]  = true, -- Jack-o'-Lanterned! [Weighted Jack-o'-Lantern]
+			[24712]  = true, -- Leper Gnome Costume
+			[61819]  = true, -- Manabonked! [The Mischief Maker]
+			[61834]  = true, -- Manabonked! [cast by Minigob Manabonk]
+			[69285]  = true, -- Mohawked! -- check 58493
+			[42365]  = true, -- Murloc Costume
+			[24710]  = true, -- Ninja Costume
+			[60106]  = true, -- Old Spices
+			[24709]  = true, -- Pirate Costume
+			[26157]  = true, -- PX-238 Winter Wondervolt
+			[44755]  = true, -- Snowflakes [Handful of Snowflakes]
+			[61815]  = true, -- Sprung! [Spring Flowers]
+			[61781]  = true, -- Turkey Feathers
 		}
-		t["Slow Fall"] = {
-			[1706]  = true, -- Levitate
-			[16593] = true, -- Noggenfogger Elixir (slow fall)
-			[130]   = true, -- Slow Fall
-		}
-		t["Water Walking"] = {
-			[11319]  = true, -- Elixir of Water Walking
+		t["Slow Fall"]  = {
 			[1706]   = true, -- Levitate
-			[546]    = true, -- Water Walking
+			[16593]  = true, -- Noggenfogger Elixir (slow fall)
+			[130]    = true, -- Slow Fall
 		}
-		t["Hellfire"] = {
-			[1949]  = 1949,  -- Hellfire [WARLOCK]
+		t["Water Walking"]  = {
+			[11319]   = true, -- Elixir of Water Walking
+			[1706]    = true, -- Levitate
+			[546]     = true, -- Water Walking
 		}
-		t["Mind Control"] = {
-			[605]   = 605, -- Mind Control [PRIEST]
+		t["Hellfire"]  = {
+			[1949]   = 1949,  -- Hellfire [WARLOCK]
 		}
-		t["Flask of Enhancement"] = {
-			[79639] = "DRUID HUNTER MONK ROGUE SHAMAN", -- Enhanced Agility
-			[79640] = "DRUID MAGE MONK PALADIN PRIEST SHAMAN WARLOCK", -- Enhanced Intellect
-			[79638] = "DEATHKNIGHT PALADIN WARRIOR", -- Enhanced Strength
+		t["Mind Control"]  = {
+			[605]    = 605, -- Mind Control [PRIEST]
+		}
+		t["Flask of Enhancement"]  = {
+			[79639]  = "DRUID HUNTER MONK ROGUE SHAMAN", -- Enhanced Agility
+			[79640]  = "DRUID MAGE MONK PALADIN PRIEST SHAMAN WARLOCK", -- Enhanced Intellect
+			[79638]  = "DEATHKNIGHT PALADIN WARRIOR", -- Enhanced Strength
 		}
 	end
 
